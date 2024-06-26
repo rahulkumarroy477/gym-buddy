693e01c83be88d55401ca27d5d0152cb18768f7a
Rahul kumar <110653984+rahulkumarroy477@users.noreply.github.com>
2024-06-26 16:57:34 +0530

    adding loader

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 import WorkoutDetail from '../components/WorkoutDetail';
 import WorkoutForm from '../components/WorkoutForm';
+
 const apiUrl = "https://gym-buddy-backend-342b.onrender.com";
+
 function Home() {
-  const {workouts, dispatch} = useWorkoutsContext();
+  const { workouts, dispatch } = useWorkoutsContext();
+  const [loading, setLoading] = useState(true); // Add loading state
+
   useEffect(() => {
-    const fetchWorkout = async ()=>{
+    const fetchWorkouts = async () => {
+      setLoading(true); // Set loading to true before fetching
       const response = await fetch(`${apiUrl}/api/workouts`);
-      const json = await response.json();   // json will be an array of docs
+      const json = await response.json(); // json will be an array of docs
 
-      if(response.ok){
-        dispatch({type:'SET_WORKOUTS',payload:json})
+      if (response.ok) {
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
       }
-    }
+      setLoading(false); // Set loading to false after fetching
+    };
 
-    fetchWorkout();
-   },
-    [dispatch]);
+    fetchWorkouts();
+  }, [dispatch]);
 
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map((workout)=>(
-          <WorkoutDetail key={workout._id} workout = {workout}>{workout.title}</WorkoutDetail>
-        ))}
+        {loading ? (
+          <div className="loader-container">
+          <div className="spinner"></div>
+        </div> 
+        ) : (
+          workouts && workouts.map((workout) => (
+            <WorkoutDetail key={workout._id} workout={workout}>
+              {workout.title}
+            </WorkoutDetail>
+          ))
+        )}
       </div>
-      <WorkoutForm/>
+      <WorkoutForm />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
